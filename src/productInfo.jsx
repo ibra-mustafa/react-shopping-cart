4874eef785936c1fa8e7e0322d0d2be54a6ec71f
Ibrahim Mustafa <ibrahimmustafa9333@gmail.com>
2021-12-03 23:27:13 +0200

    added save like functionality

--- src/productInfo.jsx
+++ src/productInfo.jsx
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
-import {useParams } from 'react-router';
+import React from 'react';
+import {Navigate, useNavigate, useParams } from 'react-router';
 const ProductInfo = (props) => {
     let{arr} = useParams()
     // console.log(props.products[0].name, arr)
     let pr = props.products.filter((x)=>{
         if( x.id === parseInt(arr) )
             return x
-        
     })
+    let savefunc = useNavigate()
     return (
     <React.Fragment>
         <h1>Info of: {pr[0].name} </h1>
         <h1>quantity: {pr[0].count} </h1>
+        <button onClick={()=>savefunc('/shoppingCart')} className="btn btn-primary btn-sm">Save</button>
     </React.Fragment>
     );
 }
