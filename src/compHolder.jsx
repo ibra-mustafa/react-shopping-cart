6b0e5e4198e98f33c3d00f2c419b82b0030becd4
Ibrahim Mustafa <ibrahimmustafa9333@gmail.com>
2021-12-10 00:55:20 +0200

    completed cart functionality

--- src/compHolder.jsx
+++ src/compHolder.jsx
@@ -5,13 +5,12 @@ import { Routes, Route, Link } from "react-router-dom";
 import ProductInfo from './productInfo';
 import NotFound from './404';
 import Table from './table';
-import Cart from './cart';
 class App extends React.Component {
     state={
         products:[
         {id:1, name:'burger',count:0, price:30,inCart: false },
-        {id:2, name:'fries',count:1, price:20,inCart: false},
-        {id:3, name:'Soda',count:2, price:10,inCart: false}
+        {id:2, name:'fries',count:0, price:20,inCart: false},
+        {id:3, name:'Soda',count:0, price:10,inCart: false}
     ]
     }
     
@@ -23,12 +22,12 @@ class App extends React.Component {
         this.setState({products})
       }
 
-    deleteHandler=(product)=>{
-        //cloning and editing the state object
-        let products = this.state.products.filter(p => product.id !==  p.id )
-        //setting the state
-        this.setState({products}) 
-    }
+    // deleteHandler=(product)=>{
+    //     //cloning and editing the state object
+    //     let products = this.state.products.filter(p => product.id !==  p.id )
+    //     //setting the state
+    //     this.setState({products}) 
+    // }
     reset=()=>{
         let products = this.state.products
         products.forEach((p)=>{p.count=0})
@@ -49,7 +48,7 @@ class App extends React.Component {
             <Routes>
                 <Route path="/shoppingCart" 
                 element={<ShoppingCart 
-                    products ={this.state.products}
+                    products ={this.state.products.filter(x=>x.inCart)}
                     reset ={this.reset} 
                     incerment={this.incerment} 
                     deleteHandler={this.handleCart}
