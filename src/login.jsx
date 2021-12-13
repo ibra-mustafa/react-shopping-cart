712f599035fccd32f08bfdf82557bb485c186988
Ibrahim Mustafa <ibrahimmustafa9333@gmail.com>
2021-12-13 10:07:02 +0200

    added client-side form validation with Joi

--- src/login.jsx
+++ src/login.jsx
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
-
+import Joi from 'joi';
 class Login extends React.Component {
     state = {
             userMail: "",
-            password:""
+            password:"",
+            errors:{}
     }
+    schema = Joi.object({
+      userMail: Joi.string()
+      .alphanum()
+      .min(3)
+      .max(30)
+      .required(),
+      password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+    })
+
     handleSubmit = (e)=>{
         e.preventDefault()
-        
+        let errors = this.validate()
+        if(errors) return;
+        console.log(errors)
     }
+
+    validate = ()=>{
+      let errors = {}
+      let stateClone = {...this.state}
+      delete stateClone.errors
+      let res = this.schema.validate(stateClone,{abortEarly:false})
+      console.log(res)
+      if(res.error == null ) return null
+      for(let error of res.error.details){
+        errors[error.path] = error.message
+      }
+      this.setState({errors})
+    }
+
     handleChange=(e)=>{
       let stateClone = {...this.state}
       stateClone[e.currentTarget.name] = e.currentTarget.value
@@ -21,12 +47,13 @@ class Login extends React.Component {
                      <div className="mb-3">
                        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
                        <input onChange={this.handleChange} name="userMail" value={this.state.userMail}  type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                       <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                      </div>
+                     {this.state.errors.userMail && (<div className="alert alert-danger">{this.state.errors.userMail}</div>)}
                      <div className="mb-3">
                        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
                        <input onChange={this.handleChange} name="password" value={this.state.password} type="password" className="form-control" id="exampleInputPassword1"/>
                      </div>
+                     {this.state.errors.password && (<div className="alert alert-danger">{this.state.errors.password}</div>)}
                      <button type="submit" onClick={this.handleSubmit} className="btn btn-primary">Submit</button>
                     </form>
         </React.Fragment>
